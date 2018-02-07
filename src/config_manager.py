0513312d72f2e0bb4ab4e2849b19f50cfba6cd01
Kacper Dębowski <kdebowski93@gmail.com>
2018-02-07 14:58:54 +0100

    Added class_name and _class_name

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import re
 from src.yaml_object import YamlObject
 
 
@@ -13,17 +14,35 @@ class ConfigManager:
             self.files = data['files']
 
     def __init__(self):
-        config_file = open('config/config.yml', 'r')
-        self.config = ConfigManager.Configuration(yaml.load(config_file))
-        config_file.close()
+        with open('config/config.yml', 'r') as config_file:
+            self.config = ConfigManager.Configuration(yaml.load(config_file))
+
         self.yaml_data = None
+        self.yaml_object = None
 
     def load_input(self, file):
-        yaml_file = open(file, 'r')
-        self.yaml_data = yaml.load(yaml_file)
-        yaml_file.close()
+        with open(file, 'r') as yaml_file:
+            self.yaml_data = yaml.load(yaml_file)
 
-        return YamlObject(self.yaml_data)
+        self.yaml_object = YamlObject(self.yaml_data)
+        return self.yaml_object
 
     def generate_files(self):
-        pass
+        for filename in self.config.files:
+            class_name_regex = re.compile('{{\s?class_name\s?}}')
+            lclass_name_regex = re.compile('{{\s?_class_name\s?}}')
+
+            lower_first = lambda s: s[:1].lower() + s[1:] if s else ''
+
+            with open(os.path.join('templates', filename), 'r') as input_file:
+                with open(os.path.join(self.config.output, filename), 'w') as output_file:
+                    while True:
+                        line = input_file.readline()
+                        if line == '':
+                            break
+                        line = class_name_regex.subn(self.yaml_object.class_name.capitalize(), line)[0]
+                        line = lclass_name_regex.subn(lower_first(self.yaml_object.class_name), line)[0]
+                        output_file.write(line)
+
+
+

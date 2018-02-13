a4b5c73a4f264e18a6db875514edbc5ea88bfb0d
Kacper Dębowski <kdebowski93@gmail.com>
2018-02-13 17:34:15 +0100

    Made working output directories, fixed templates, added another test entity, made it basically work

--- crudgen.py
+++ crudgen.py
@@ -1,6 +1,13 @@
+import argparse
 from src.config_manager import ConfigManager
 
 if __name__ == "__main__":
-    cfg = ConfigManager()
-    obj = cfg.load_input('input/person.yml')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', type=str)
+    parser.add_argument('output_root', type=str)
+
+    args = parser.parse_args()
+
+    cfg = ConfigManager(args.output_root)
+    obj = cfg.load_input(args.input_file)
     cfg.generate_files()
--- src/config_manager.py
+++ src/config_manager.py
@@ -13,13 +13,15 @@ class ConfigManager:
             self.output = data['output']
             self.files = data['files']
             self.output_names = data['output_names']
+            self.output_dirs = data['output_directories']
 
-    def __init__(self):
+    def __init__(self, output_root):
         with open('config/config.yml', 'r') as config_file:
             self.config = ConfigManager.Configuration(yaml.load(config_file))
 
         self.yaml_data = None
         self.yaml_object = None
+        self.output_root = output_root
 
     def load_input(self, file):
         with open(file, 'r') as yaml_file:
@@ -29,6 +31,11 @@ class ConfigManager:
         return self.yaml_object
 
     def generate_files(self):
+        def lower_first(s): return s[:1].lower() + s[1:] if s else ''
+
+        def upper_first(s): return s[:1].upper() + s[1:] if s else ''
+
+
         class_name_regex = re.compile('{{\s?class_name\s?}}')
         lclass_name_regex = re.compile('{{\s?_class_name\s?}}')
         plural_class_name_regex = re.compile('{{\s?class_name_plural\s?}}')
@@ -37,20 +44,28 @@ class ConfigManager:
         end_regex = re.compile('{{\s?end\s?}}')
         field_regex = re.compile('{{\s?field\s?}}')
         type_regex = re.compile('{{\s?type\s?}}')
+        uppercase_type_regex = re.compile('{{\s?\^\s?type\s?}}')
+        output_path = os.path.join(self.config.output, self.output_root)
+
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+
+        for final_dir in self.config.output_dirs:
+            if not os.path.exists(os.path.join(output_path, final_dir)):
+                os.mkdir(os.path.join(output_path, final_dir))
 
         for filename in self.config.files:
-            def lower_first(s): return s[:1].lower() + s[1:] if s else ''
 
             with open(os.path.join('templates', filename), 'r') as input_file:
-                with open(os.path.join(self.config.output, filename), 'w') as output_file:
+                with open(os.path.join(output_path, filename), 'w') as output_file:
                     while True:
                         line = input_file.readline()
                         if line == '':
                             break
 
-                        line = class_name_regex.subn(self.yaml_object.class_name.capitalize(), line)[0]
+                        line = class_name_regex.subn(upper_first(self.yaml_object.class_name), line)[0]
                         line = lclass_name_regex.subn(lower_first(self.yaml_object.class_name), line)[0]
-                        line = plural_class_name_regex.subn(self.yaml_object.plural_class_name.capitalize(), line)[0]
+                        line = plural_class_name_regex.subn(upper_first(self.yaml_object.plural_class_name), line)[0]
                         line = plural_lclass_name_regex.subn(lower_first(self.yaml_object.plural_class_name), line)[0]
 
                         if foreach_field_regex.search(line) is not None:
@@ -67,19 +82,20 @@ class ConfigManager:
                                     if end_regex.search(line) is not None:
                                         line = ''
                                         break
-                                    line = class_name_regex.subn(self.yaml_object.class_name.capitalize(), line)[0]
+                                    line = class_name_regex.subn(upper_first(self.yaml_object.class_name), line)[0]
                                     line = lclass_name_regex.subn(lower_first(self.yaml_object.class_name), line)[0]
-                                    line = plural_class_name_regex.subn(self.yaml_object.plural_class_name.capitalize(), line)[0]
+                                    line = plural_class_name_regex.subn(upper_first(self.yaml_object.plural_class_name), line)[0]
                                     line = plural_lclass_name_regex.subn(lower_first(self.yaml_object.plural_class_name), line)[0]
                                     line = field_regex.subn(field.name, line)[0]
                                     line = type_regex.subn(field.type, line)[0]
+                                    line = uppercase_type_regex.subn(upper_first(field.type), line)[0]
                                     output_file.write(line)
 
                         output_file.write(line)
 
         for index in range(0, len(self.config.files)):
-            filename = class_name_regex.subn(self.yaml_object.class_name.capitalize(), self.config.output_names[index])[0]
-            os.rename(os.path.join('output', self.config.files[index]), os.path.join('output', filename))
+            filename = class_name_regex.subn(upper_first(self.yaml_object.class_name), self.config.output_names[index])[0]
+            os.rename(os.path.join(output_path, self.config.files[index]), os.path.join(output_path, self.config.output_dirs[index], filename))
 
 
 
